cc2ab7fa0ef333b159c08ece92360d230ccf1dc2
Faisal <fasinadem108@gmail.com>
2023-02-20 11:38:13 +0500

    title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>murd</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
