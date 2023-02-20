48d20553f1a1694d4546b6d84c2498bc29573603
Faisal <fasinadem108@gmail.com>
2023-02-20 11:40:43 +0500

    title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>surdeeee</title>
+        <title>murd</title>
       </Head>
       <body>
         <Main />
